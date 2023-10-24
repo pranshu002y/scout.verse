edc3aaeb2b720c994486e645561c1926696792ae
Pranshu Yadav <pranshu.yadav@venturepact.com>
2023-10-24 16:50:26 +0530

    images changed

--- src/Posts.js
+++ src/Posts.js
@@ -3,21 +3,21 @@ const Posts = ()=>{
     return(
         <div id="page3">
             <div className="child" id="child1">
-                <img src="https://cdn.sanity.io/images/w8f1ak3c/production/d6a2a4be8e3063d64648773f57f5f447609a93ab-5000x5000.png/Love-Care-Pack-Expanded-Two-Good-Co.png?w=1024&h=1024&auto=format"
+                <img src="https://images.unsplash.com/photo-1623691183627-4d6075a8d0d7?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                     alt=""/>
             </div>
             <div className="child" id="child2">
-                <img src="https://cdn.sanity.io/images/w8f1ak3c/production/d6a2a4be8e3063d64648773f57f5f447609a93ab-5000x5000.png/Love-Care-Pack-Expanded-Two-Good-Co.png?w=1024&h=1024&auto=format"
+                <img src="https://assets.beartai.com/uploads/2023/02/MV5BODhkN2U1YzYtODQzZC00MTc5LTlmMmYtYjQ2ZGU2ZmM4YzJkXkEyXkFqcGdeQXVyMTE0MTc4MjU2._V1_FMjpg_UX1000_-768x1024.jpg"
                     alt=""/>
 
             </div>
             <div className="child" id="child3">
-                <img src="https://cdn.sanity.io/images/w8f1ak3c/production/d6a2a4be8e3063d64648773f57f5f447609a93ab-5000x5000.png/Love-Care-Pack-Expanded-Two-Good-Co.png?w=1024&h=1024&auto=format"
+                <img src="https://media.contentapi.ea.com/content/dam/eacom/NFSNL/Mosaic/FeatureImg_LTS_Art_648x648.png.adapt.crop1x1.png"
                     alt=""/>
 
             </div>
             <div className="child" id="child4">
-                <img src="https://cdn.sanity.io/images/w8f1ak3c/production/d6a2a4be8e3063d64648773f57f5f447609a93ab-5000x5000.png/Love-Care-Pack-Expanded-Two-Good-Co.png?w=1024&h=1024&auto=format"
+                <img src="https://w0.peakpx.com/wallpaper/1004/457/HD-wallpaper-battlegrounds-mobile-gun-game-fire-war-bullet-india-pubg.jpg"
                     alt=""/>
 
             </div>
